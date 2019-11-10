b33ad7fff6e77eef72d420f326a14a5a8f978014
hound672 <hound672@gmail.com>
2019-11-10 12:56:11 +0300

    Initial commit

---
+++ init_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter project name: " project_name
+read -p "Enter project description: " project_description
+read -p "Enter project author name: " project_author_name
+read -p "Enter project author email: " project_author_email
+
+
+sed -i "s~PROJECT_NAME~$project_name~g" Makefile setup.cfg pyproject.toml
+sed -i "s~PROJECT_AUTHOR_NAME~$project_author_name~g" pyproject.toml
+sed -i "s~PROJECT_AUTHOR_EMAIL~$project_author_email~g" pyproject.toml
+sed -i "s~PROJECT_DESCRIPTION~$project_description~g" pyproject.toml
+
+rm init_project.sh
+
